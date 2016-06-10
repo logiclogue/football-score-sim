be8c5cf15de98ff530eb8c68d80ea17a35d0dbb2
logiclogue <jordanpaullord@hotmail.co.uk>
2016-06-10 19:07:39 +0100

    Fixes test

--- js/test.js
+++ js/test.js
@@ -1,11 +1,13 @@
-var Match = require('./js/Match');
-var Team = require('./js/Team');
+var Match = require('./Match');
+var Team = require('./Team');
 
 var england = new Team('England', 1947);
 var russia = new Team('Russia', 1736);
 var wales = new Team('Wales', 1629);
 var slovakia = new Team('Slovakia', 1742);
-var match = new Match(slovakia, wales);
+var derby = new Team('Derby County', 1544);
+var united = new Team('Manchester United', 1794);
+var match = new Match(derby, united);
 
 
 /*
