d6ee854548fa61aab40a0d8aad4e505293567b15
logiclogue <jordanpaullord@hotmail.co.uk>
2016-06-23 21:47:07 +0100

    Adds class MatchWithGoalTimes

---
+++ js/MatchWithGoalTimes.js
@@ -0,0 +1,14 @@
+/*
+ * Class which extends Match and adds goal times.
+ */
+function MatchWithGoalTimes() {
+
+}
+
+(function (static_, proto_) {
+
+    
+
+}(MatchWithGoalTimes, MatchWithGoalTimes.prototype));
+
+module.exports = MatchWithGoalTimes;
