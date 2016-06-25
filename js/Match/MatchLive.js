6563de571345e9a497c9cb02dcd374f3f0ecfc5d
logiclogue <jordanpaullord@hotmail.co.uk>
2016-06-25 22:06:47 +0100

    Updates MatchLive class with match period object and calculate method

--- js/Match/MatchLive.js
+++ js/Match/MatchLive.js
@@ -7,11 +7,32 @@ var MatchWithGoalTimes = require('./MatchWithGoalTimes');
  * the match at the current time.
  * Extends MatchWithGoalTimes class.
  */
-function MatchLive() {
+function MatchLive(teamA, teamB, options) {
     MatchWithGoalTimes.apply(this, arguments);
+
+    this.period = {
+        preKickOff: 0,
+        firstHalf: 1,
+        halfTime: 2,
+        secondHalf: 3,
+        preExtraTime: 4,
+        extraTimeFirstHalf: 5,
+        extraTimeHalfTime: 6,
+        extraTimeSecondHalf: 7,
+        prePenalties: 8,
+        penalties: 9,
+        fullTime: 10
+    };
+    this.startTime = new Date(options.startTime);
+    this.matchLength = options.matchLength || 90;
+    this.currentPeriod = this.period.preKickOff;
+    
+    // Overrides:
+    this.goals;
+    this.goalTimes;
 }
 
-// Extending.
+// Extending MatchWithGoalTimes class.
 MatchLive.prototype = Object.create(MatchWithGoalTimes.prototype);
 
 (function (static_, proto_, super_) {
@@ -23,6 +44,18 @@ MatchLive.prototype = Object.create(MatchWithGoalTimes.prototype);
         var returnVal = super_.simulate.call(this);
 
         return returnVal;
+    };
+
+
+    /*
+     * Calculates what period of the game it is.
+     */
+    proto_._calculatePeriod = function () {
+        var currentTime = new Date();
+
+        if (currentTime.getTime() < this.startTime.getTime()) {
+            this.currentPeriod = this.period.preKickOff;
+        }
     };
 
 }(MatchLive, MatchLive.prototype, MatchWithGoalTimes.prototype));
