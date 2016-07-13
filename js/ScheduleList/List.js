40138270fa6dcf225685204bb097e51586a93dfc
logiclogue <jordanpaullord@hotmail.co.uk>
2016-07-13 10:25:24 +0100

    Adds List class

---
+++ js/ScheduleList/List.js
@@ -0,0 +1,55 @@
+/*
+ * Create a list with manages list items.
+ */
+function List() {
+    this.items = [];
+    this.currentTime = Date.now();
+}
+
+(function (static_, proto_) {
+
+    /*
+     * Adds an item to the list.
+     * Parameter is item.
+     */
+    proto_.addItem = function (item) {
+        this.items.push(item);
+        this.items.sort(this._sortFunction.bind(this));
+    };
+
+    /*
+     * Executes all item callback functions that
+     * should've happened now.
+     */
+    proto_.run = function () {
+        this.currentTime = Date.now();
+
+        this.items.forEach(this._forEachItemRun.bind(this));
+    };
+
+
+    /*
+     * For each item execute it if the time has
+     * passed.
+     */
+    proto_._forEachItemRun = function (item) {
+        if (item.startTime < this.currentTime) {
+            item.callback();
+
+            item.executed = true;
+        }
+    };
+
+    /*
+     * Sorts the items in order by their start times.
+     */
+    proto_._sortFunction = function (itemA, itemB) {
+        if (itemA.startTime > itemB.startTime)
+            return true;
+
+        return false;
+    };
+
+}(List, List.prototype));
+
+module.exports = List;
