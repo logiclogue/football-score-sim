7489b48b11b4af2918170ed7740b15032c6122cc
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-03 22:48:10 +0100

    Adds graph to GoalGenerator

--- src/GoalGenerator.js
+++ src/GoalGenerator.js
@@ -1,19 +1,31 @@
+var Goal = require('./Goal');
+var NormalDistribution = require('./NormalDistribution');
+
+
 /*
  * Generates the number of goals scored by a team in a match.
  */
 function GoalGenerator(options) {
     // Classes
-    this.Goal = options.Goal;
+    this.Goal = options.Goal || Goal;
+    this.NormalDistribution = options.NormalDistribution || NormalDistribution;
 
     // Instances
     this.period = options.period;
     this.teamScoring = options.teamScoring;
     this.teamConceding = options.teamConceding;
     this.random = options.random;
+    this.graph;
 
     // Variables
     this.seed = options.seed;
     this.ratingDifference = options.ratingDifference;
+    this.mean = 1.58;
+    this.standardDeviation = 1.23;
+    this.constant = 0.00245;
+
+    //
+    this._calculateGraph();
 }
 
 (function (proto_) {
@@ -29,6 +41,7 @@ function GoalGenerator(options) {
         var length = this.period.length;
         var ratingDifference = this._getRatingDifference();
         var rand = this.random.decimal(seed);
+        var goals = 0;
     };
 
 
@@ -37,6 +50,16 @@ function GoalGenerator(options) {
      */
     proto_._getRatingDifference = function () {
         return this.teamScoring.rating - this.teamConceding.rating;
+    };
+
+    /*
+     * Calculates the graph.
+     */
+    proto_._calculateGraph = function () {
+        var prodRatingDiffConstant = this.ratingDifference * this.constant;
+        var mean = this.mean + prodRatingDiffConstant;
+
+        this.graph = new this.NormalDistribution(mean, this.standardDeviation);
     };
 
 }(GoalGenerator.prototype));
