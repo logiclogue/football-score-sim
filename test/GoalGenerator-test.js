5bdd685d7cba310d30787ca7d91580394d2e8c48
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-05 14:20:09 +0100

    Updates GoalGenerator tests

--- test/GoalGenerator-test.js
+++ test/GoalGenerator-test.js
@@ -1,18 +1,32 @@
+var assert = require('chai').assert;
 var GoalGenerator = require('../src/GoalGenerator');
-var Team = require('../src/Team');
-var Period = require('../src/Period');
+var Goal = require('../src/Goal');
 var common = require('./common');
 
 
 describe('GoalGenerator', function () {
+    var seed = 'test';
     var goalGenerator = new GoalGenerator({
-        seed: 'test' + Math.random(),
+        seed: seed,
         teamScoring: common.teamEngland,
         teamConceding: common.teamSlovakia,
         period: common.fullMatchPeriod
     });
 
     describe('#generator()', function () {
-        console.log(goalGenerator.generate());
+        var goals = goalGenerator.generate();
+
+        it('should return 1 goal with seed ' + seed, function () {
+            assert.equal(goals.length, 1);
+        });
+
+        it('should return type array', function () {
+            assert.typeOf(goals, 'array');
+        });
+
+        it('should return a goal that is instance of Goal', function () {
+            assert.instanceOf(goals[0], Goal);
+            assert.instanceOf(goals[goals.length - 1], Goal);
+        });
     });
 });
