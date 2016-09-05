fef5173610fda06bfe8b77e3b72e17c4dab1f03c
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-05 14:43:56 +0100

    Removes TeamGoals class

--- src/Period.js
+++ src/Period.js
@@ -1,19 +1,13 @@
 var Goal = require('./Goal');
-var TeamGoals = require('./TeamGoals');
 
 
 function Period(options) {
     // Classes
     this.Goal = options.Goal || Goal;
-    this.TeamGoals = options.TeamGoals || TeamGoals;
 
     // Variables
     this.length = options.length || 2700000; // or 45 minutes in milliseconds
     this.startTime = options.startTime || Date.now();
-    /*this.goals = [
-        new options.TeamGoals(options),
-        new options.TeamGoals(options)
-    ];*/
 }
 
 module.exports = Period;
--- src/TeamGoals.js
+++
@@ -1,8 +0,0 @@
-function TeamGoals(options) {
-    this.Goal = options.Goal;
-    
-    this.team = options.team;
-    this.goals = [];
-}
-
-module.exports = TeamGoals;
