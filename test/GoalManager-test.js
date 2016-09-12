4c7367da26f2340a65c79c92f08b0c9adc8fba9d
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-12 14:15:05 +0100

    Adds more tests for GoalManager

--- test/GoalManager-test.js
+++ test/GoalManager-test.js
@@ -5,11 +5,17 @@ var GoalManager = require('../src/GoalManager');
 
 describe('GoalManager', function () {
     var goalManager;
+    var goalManager2;
 
     describe('#constructor(options)', function () {
         goalManager = new GoalManager({
             teamA: common.teamEngland,
             teamB: common.teamSlovakia,
+            period: common.fullMatchPeriod
+        });
+        goalManager2 = new GoalManager({
+            teamA: common.teamSlovakia,
+            teamB: common.teamEngland,
             period: common.fullMatchPeriod
         });
 
@@ -24,10 +30,33 @@ describe('GoalManager', function () {
     });
 
     describe('#addGoals(teamIndex, goals)', function () {
+        it('should add the goals to #goals', function () {
+            var goals = common.goalGenerator.generate();
 
+            goalManager.addGoals(0, goals);
+
+            assert.deepEqual(goalManager.goals[0], goals);
+        });
     });
 
     describe('#append(goalManager)', function () {
+        it('should not throw error', function () {
+            var append = goalManager.append.bind(goalManager, goalManager);
+            
+            assert.doesNotThrow(append, ReferenceError);
+        });
 
+        it('should append the goals', function () {
+            var goals = goalManager.goals;
+
+            assert.equal(goals[0].length, 2);
+            assert.equal(goals[1].length, 0);
+        });
+
+        it('should throw error, if teams are reversed', function () {
+            var append = goalManager.append.bind(goalManager, goalManager2);
+
+            assert.throws(append, ReferenceError);
+        });
     });
 });
--- test/common.js
+++ test/common.js
@@ -1,18 +1,34 @@
 var Team = require('../src/Team');
 var Period = require('../src/Period');
+var GoalGenerator = require('../src/GoalGenerator');
 
+
+var teamEngland = new Team({
+    name: 'England',
+    rating: 1891
+});
+
+var teamSlovakia = new Team({
+    name: 'Slovakia',
+    rating: 1736
+});
+
+
+var fullMatchPeriod = new Period({
+    length: 90 * 60 * 1000
+});
+
+
+var goalGenerator = new GoalGenerator({
+    period: fullMatchPeriod,
+    teamScoring: teamEngland,
+    teamConceding: teamSlovakia,
+    seed: 'test'
+});
 
 module.exports = {
-    teamEngland: new Team({
-        name: 'England',
-        rating: 1891
-    }),
-    teamSlovakia: new Team({
-        name: 'Slovakia',
-        rating: 1736
-    }),
-
-    fullMatchPeriod: new Period({
-        length: 90 * 60 * 1000
-    })
+    teamEngland: teamEngland,
+    teamSlovakia: teamSlovakia,
+    fullMatchPeriod: fullMatchPeriod,
+    goalGenerator: goalGenerator
 };
