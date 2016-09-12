ec788f3d7244f3d959394b6fd21fd7cc06689aff
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-12 11:43:21 +0100

    Adds method #append and #addGoals to GoalManager

--- src/GoalManager.js
+++ src/GoalManager.js
@@ -21,8 +21,31 @@ function GoalManager(options) {
 
 (function (proto_) {
     
-    proto_.addGoals = function () {
+    /*
+     * Appends goals, that are passed in, to the this.goals array, depending on
+     * the team index given.
+     */
+    proto_.addGoals = function (teamIndex, goals) {
+        var goalsArray = this.goals[teamIndex];
 
+        goalsArray.push.apply(goalsArray, goals);
+    };
+
+    /*
+     * Appends all the goals, from the passed in GoalManager, to the goals in
+     * this instance of GoalManager.  Doesn't pass in if teams, in the passed
+     * in GoalManager and current GoalManager instance, are different.
+     */
+    proto_.append = function (goalManager) {
+        var isNotTeamA = goalManager.teams[0] !== this.teams[0];
+        var isNotTeamB = goalManager.teams[1] !== this.teams[1];
+
+        if (isNotTeamA || isNotTeamB) {
+            throw new ReferenceError('teams are different');
+        }
+
+        this.addGoals(0, goalManager.goals[0]);
+        this.addGoals(1, goalManager.goals[1]);
     };
 
 }(GoalManager.prototype));
