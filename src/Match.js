19b4d6f11ddb13a3d2da01d5c8f476d6d1d19011
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-13 20:08:42 +0100

    Adds #simulate method

--- src/Match.js
+++ src/Match.js
@@ -17,10 +17,35 @@ function Match(options) {
         teamA: options.teamA,
         teamB: options.teamB
     });
+    this.firstHalf = new this.Period({
+        teamA: options.teamA,
+        teamB: options.teamB,
+        length: 45 * 60000
+        seed: 'firstHalf'
+    });
+    this.secondHalf = new this.Period({
+        teamA: options.teamA,
+        teamB: options.teamB,
+        length: 45 * 60000,
+        seed: 'secondHalf'
+    });
+    this.extraTimeFirstHalf = new this.Period({
+        teamA: options.teamA,
+        teamB: options.teamB,
+        length: 15 * 60000,
+        seed: 'extraTime firstHalf'
+    });
+    this.extraTimeSecondHalf = new this.Period({
+        teamA: options.teamA,
+        teamB: options.teamB,
+        length: 15 * 60000,
+        seed: 'extraTime secondHalf'
+    });
     
     // Variables
     this.extraTime = options.extraTime || false;
     this.penalties = options.penalties || false;
+    this.seed = options.seed;
 }
 
 (function (proto_) {
@@ -30,7 +55,17 @@ function Match(options) {
      * Returns the match score.
      */
     proto_.simulate = function () {
-        
+        this.firstHalf.simulate();
+        this.secondHalf.simulate();
+        this.extraTimeFirstHalf.simulate();
+        this.extraTimeSecondHalf.simulate();
+
+        this.goalManager.append(this.firstHalf.goalManager);
+        this.goalManager.append(this.secondHalf.goalManager);
+        this.goalManager.append(this.extraTimeFirstHalf.goalManager);
+        this.goalManager.append(this.extraTimeSecondHalf.goalManager);
+
+        return this.goalManager.getScore();
     };
 
 }(Match.prototype));
