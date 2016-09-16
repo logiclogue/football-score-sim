133a5f533dba50255812d5d9fcd6e44ccfb67e92
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-16 23:25:39 +0100

    Adds text output for extra time

--- src/MatchOutputter.js
+++ src/MatchOutputter.js
@@ -17,14 +17,26 @@ function MatchOutputter(options) {
     };
 
     proto_.scoreEachHalf = function () {
-        var teamA = this.match.teamA.name;
-        var teamB = this.match.teamB.name;
-        var firstHalf = this.match.firstHalf.goalManager.getScore();
-        var secondHalf = this.match.secondHalf.goalManager.getScore();
+        var match = this.match;
+        var teamA = match.teamA.name;
+        var teamB = match.teamB.name;
+        var firstHalf = match.firstHalf.goalManager.getScore();
+        var secondHalf = match.secondHalf.goalManager.getScore();
+        var firstHalfET = match.extraTimeFirstHalf.goalManager.getScore();
+        var secondHalfET = match.extraTimeSecondHalf.goalManager.getScore();
         
-        return teamA + ' vs ' + teamB + '\n' +
+        var output = teamA + ' vs ' + teamB + '\n' +
             'First half: ' + firstHalf[0] + '-' + firstHalf[1] + '\n' +
-            'Second half: ' + secondHalf[0] + '-' + secondHalf[1];
+            'Second half: ' + secondHalf[0] + '-' + secondHalf[1] + '\n';
+
+        if (!match.wentToExtraTime) {
+            return output;
+        }
+
+        output += 'ET First half: ' + firstHalfET[0] + '-' + firstHalfET[1] + '\n' +
+            'ET Second Half: ' + secondHalfET[0] + '-' + secondHalfET[1] + '\n';
+
+        return output;
     };
 
 }(MatchOutputter.prototype));
