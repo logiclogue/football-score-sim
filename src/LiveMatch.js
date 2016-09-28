02acc603f07b5e8d79d22c7093c7cc434a4f921b
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-28 11:13:45 +0100

    Adds LiveMatch constructor

--- src/LiveMatch.js
+++ src/LiveMatch.js
@@ -1,5 +1,30 @@
-function LiveMatch() {
+var Match = require('./Match');
+var ScheduleList = require('./ScheduleList');
+var ScheduleListItem = require('./ScheduleListItem');
 
+
+function LiveMatch(options) {
+    Match.call(this, options);
+    
+    // Classses
+    this.Match = options.Match || Match;
+    this.ScheduleList = options.ScheduleList || ScheduleList;
+    this.ScheduleListItem = options.ScheduleListItem || ScheduleListItem;
+
+    // Instances
+    this.goalList = new this.ScheduleList();
+
+    // Variables
 }
+
+LiveMatch.prototype = Object.create(Match.prototype);
+
+(function (proto_, super_) {
+
+    proto_.simulate = function () {
+        return super_.simulate.call(this);
+    };
+
+}(LiveMatch.prototype, Match.prototype));
 
 module.exports = LiveMatch;
