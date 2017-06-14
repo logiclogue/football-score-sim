83066f4f84441b47ab93e642f5d64c4f644d2378
logiclogue <jordanpaullord@hotmail.co.uk>
2017-06-14 21:19:52 +0100

    Adds empty PenaltiesEvents class

---
+++ src/PenaltiesEvents.js
@@ -0,0 +1,47 @@
+var TimeEvents = require('./TimeEvents');
+var PeriodEvents = require('./Period');
+
+function PenaltiesEvents(options) {
+    this.penalties = options.penalties;
+    this.periodEvents = options.periodEvents || new PeriodEvents({
+        period: penalties
+    });
+}
+
+(function (proto_) {
+
+    /*
+     * Calls the callback with the goal object when a penalty shootout goal is
+     * scored.
+     */
+    proto_.onGoal = function () {
+        
+    };
+
+    /*
+     * Calls the callback when a penalty is missed, with a goal object.
+     * The goal object behaves as a miss to show which team missed.
+     */
+    proto_.onMiss = function () {
+        
+    };
+
+    /*
+     * Calls the callback when the penalty shootout starts.
+     * It uses the internal PeriodEvents object.
+     */
+    proto_.onStart = function () {
+        
+    };
+
+    /*
+     * Calls the callback went the penalty shootout finished.
+     * It uses the internal PeriodEvents object.
+     */
+    proto_.onFinish = function () {
+        
+    };
+    
+}(PenaltiesEvents.prototype));
+
+module.exports = PenaltiesEvents;
