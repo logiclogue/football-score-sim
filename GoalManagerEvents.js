f986565e4c5288d08bc34f6978042efa6ce0f3a0
logiclogue <jordanpaullord@hotmail.co.uk>
2017-06-24 22:37:40 +0100

    Adds empty GoalManagerEvents

---
+++ GoalManagerEvents.js
@@ -0,0 +1,13 @@
+var GoalManager = require('./GoalManager');
+
+function GoalManagerEvents(options) {
+    this.goalManager = options.goalManager || new GoalManager();
+}
+
+(function (proto_) {
+
+
+    
+}(GoalManagerEvents.prototype));
+
+module.exports = GoalManagerEvents;
