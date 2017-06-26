6a503f118257b816433a8f0b3387d219c194efd5
logiclogue <jordanpaullord@hotmail.co.uk>
2017-06-26 19:20:20 +0100

    Adds skeleton methods in MatchEvents

--- src/MatchEvents.js
+++ src/MatchEvents.js
@@ -28,6 +28,71 @@ function MatchEvents(options) {
     };
 
     /*
+     * Call the callback when it's half-time.
+     */
+    proto_.onHalfTime = function (callback) {
+        
+    };
+
+    /*
+     * Call the callback when the second half starts.
+     */
+    proto_.onSecondHalfKickOff = function (callback) {
+        
+    };
+
+    /*
+     * Call the callback when the 90 minutes has ended, but there is still
+     * extra time to be played.
+     */
+    proto_.onEndOf90Mins = function (callback) {
+        
+    };
+
+    /*
+     * Call the callback when the first half of extra time is kicked off.
+     */
+    proto_.onExtraTimeKickOff = function (callback) {
+        
+    };
+
+    /*
+     * Call the callback when it's half time extra time.
+     */
+    proto_.onHalfTimeExtraTime = function (callback) {
+        
+    };
+
+    /*
+     * Call the callback when second half extra time kicks off.
+     */
+    proto_.onSecondHalfETKickOff = function (callback) {
+        
+    };
+
+    /*
+     * Call the callback when the 120 minutes have been played, but there is
+     * still penalties.
+     */
+    proto_.onEndOf120Mins = function (callback) {
+        
+    };
+
+    /*
+     * Call the callback when the penalty shootout is kicked off.
+     */
+    proto_.onPenaltyShootoutKickOff = function () {
+        
+    };
+
+    /*
+     * Call the callback when there has been a goal in the penalty shootout.
+     */
+    proto_.onPenaltyShootoutGoal = function () {
+        
+    };
+
+    /*
      * Call the callback when the match ends.
      */
     proto_.onFullTime = function (callback) {
