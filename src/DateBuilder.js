548d669cf74ca04cd0d06140409d101d787eb918
logiclogue <jordanpaullord@hotmail.co.uk>
2017-07-08 11:29:25 +0100

    Removes setters in place for options in the object constructor

--- src/DateBuilder.js
+++ src/DateBuilder.js
@@ -1,50 +1,19 @@
 var Seed = require('./Seed');
 
 function DateBuilder(options) {
-    this.minMilliDelta = 0;
-    this.maxMilliDelta = 0;
-    this.meanDate = new Date();
+    this.minMilliDelta = options.minMilliDelta || 0;
+    this.maxMilliDelta = options.maxMilliDelta || 0;
+    this.meanDate = options.meanDate || new Date();
     this.seed = new Seed();
-    this.seed.setValue(this.meanDate.toString());
+    this.seed = options.seed ||
+        new Seed()
+            .setValue(this.meanDate.toString());
     this.setFixedPointDate = this.meanDate;
-    this.standardDeviationMilli = 0;
+    this.fixedPointDate = options.fixedPointDate || this.meanDate;
+    this.standardDeviationMilli = options.standardDeviationMilli || 0;
 }
 
 (function (proto_) {
-
-    proto_.setMinimumMilliDelta = function (minMilliDelta) {
-        this.minMilliDelta = minMilliDelta;
-
-        return this;
-    };
-
-    proto_.setMaximumMilliDelta = function (maxMilliDelta) {
-        this.maxMilliDelta = maxMilliDelta;
-
-        return this;
-    };
-
-    proto_.setMeanDate = function (meanDate) {
-        this.meanDate = meanDate;
-
-        return this;
-    };
-
-    proto_.setFixedPointDate = function (fixedPointDate) {
-        this.setFixedPointDate = fixedPointDate;
-
-        return this;
-    };
-
-    proto_.setStandardDeviationDate = function (standardDeviationDate) {
-        this.standardDeviationMilli = standardDeviationMilli;
-
-        return this;
-    };
-
-    proto_.setSeed = function (seed) {
-        this.seed = seed;
-    };
 
     proto_.getResult = function () {
         
