65ff47865875fd1e6cc233658195671f4628e32f
logiclogue <jordanpaullord@hotmail.co.uk>
2017-07-11 08:41:24 +0100

    Removes setters on StartFinishDates

--- src/StartFinishDates.js
+++ src/StartFinishDates.js
@@ -1,28 +1,10 @@
-function StartFinishDates() {
-    this.startDate;
-    this.finishDate;
-    this.milliLength = 0;
+function StartFinishDates(startDate, finishDate, timeLength) {
+    this.startDate = startDate;
+    this.finishDate = finishDate;
+    this.timeLength = timeLength;
 }
 
 (function (proto_) {
-
-    proto_.setStartDate = function (startDate) {
-        this.startDate = startDate;
-
-        return this;
-    };
-
-    proto_.setFinishdate = function (finishDate) {
-        this.finishDate = finishDate;
-
-        return this;
-    };
-
-    proto_.setMilliLength = function (milliLength) {
-        this.milliLength = milliLength;
-
-        return this;
-    };
 
     proto_.getMilliLength = function () {
         
