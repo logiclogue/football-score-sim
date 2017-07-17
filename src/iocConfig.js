6b694fe7d4e01a3e3c97ca391b2df98ed4292fad
logiclogue <jordanpaullord@hotmail.co.uk>
2017-07-17 12:39:26 +0100

    Updates iocConfig

--- src/iocConfig.js
+++ src/iocConfig.js
@@ -2,6 +2,6 @@ var TrapeziumRule = require('./TrapeziumRule');
 
 module.exports = {
 
-    calculateArea: new TrapeziumRule(0.1).getArea,
+    calculateArea: new TrapeziumRule(0.1, 10000).getArea,
 
 };
