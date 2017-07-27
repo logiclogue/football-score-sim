0e59b46de9d1c242f22cc35bcd189474c850c1bd
logiclogue <jordanpaullord@hotmail.co.uk>
2017-07-27 21:04:21 +0100

    Adds empty PeriodFactory

---
+++ src/PeriodFactory.js
@@ -0,0 +1,25 @@
+function PeriodFactory() {
+    
+}
+
+PeriodFactory.prototype = {
+
+    createFirstHalf: function (seed, previousPeriod) {
+
+    },
+
+    createSecondHalf: function (seed, previousPeriod) {
+
+    },
+
+    createFirstHalfET: function (seed, previousPeriod) {
+
+    },
+
+    createSecondHalfET: function (seed, previousPeriod) {
+
+    }
+
+};
+
+module.exports = PeriodFactory;
