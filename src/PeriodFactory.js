52cc39a7908557ed69e72137f88dde3c97a1ff0f
logiclogue <jordanpaullord@hotmail.co.uk>
2017-07-28 07:24:47 +0100

    Updates PeriodFactory, failing

--- src/PeriodFactory.js
+++ src/PeriodFactory.js
@@ -2,14 +2,49 @@ var Period = require('./Period');
 var Time = require('./Time');
 
 function PeriodFactory() {
-    
+    this.teamA;
+    this.teamB;
+    this.seed;
 }
 
 PeriodFactory.prototype = {
 
-    create: function (teamA, teamB, timeLength, seed, previousPeriod, timeAfterPrevious) {
-        $
-    }
+    setTeamA: function (teamA) {
+        var periodFactory = new PeriodFactory();
+
+        periodFactory.teamA = teamA;
+        periodFactory.teamB = this.teamB;
+        periodFactory.seed = this.seed;
+
+        return periodFactory;
+    },
+
+    setTeamB: function (teamB) {
+        var periodFactory = new PeriodFactory();
+
+        periodFactory.teamA = this.teamA;
+        periodFactory.teamB = teamB;
+        periodFactory.seed = this.seed;
+
+        return periodFactory;
+    },
+
+    setTeamB: function (seed) {
+        var periodFactory = new PeriodFactory();
+
+        periodFactory.teamA = this.teamA;
+        periodFactory.teamB = this.teamB;
+        periodFactory.seed = seed;
+
+        return periodFactory;
+    },
+
+    create: function (options) {
+        return new Period({
+            timeLength: new Time(options.minutesLength * 60 *1000),
+            previousPeriod: options.previousPeriod
+        });
+    },
 
     createFirstHalf: function (seed, previousPeriod) {
         return new Period({
--- test/PeriodFactory-test.js
+++ test/PeriodFactory-test.js
@@ -1,6 +1,8 @@
 var assert = require('chai').assert;
 var PeriodFactory = require('../src/PeriodFactory');
 var Seed = require('../src/Seed');
+var Period = require('../src/Period');
+var Time = require('../src/Time');
 var common = require('./common');
 
 describe('PeriodFactory', function () {
@@ -12,7 +14,43 @@ describe('PeriodFactory', function () {
         seed = new Seed('test', ',');
     });
 
-    describe('#createFirstHalf', function () {
+    describe('#create()', function () {
+        var result;
+        var minutesLength;
+        var period;
+        var minutesAfterPrevious;
+
+        beforeEach(function () {
+            minutesLength = 15;
+            minutesAfterPrevious = 5;
+            period = new Period({});
+
+            result = periodFactory.create({
+                minutesLength: minutesLength,
+                previousPeriod: period,
+                minutesAfterPrevious: minutesAfterPrevious
+            });
+        });
+
+        it('should create a period that has the correct length', function () {
+            assert.equal(result.timeLength.getMinutes(), 15);
+        });
+
+        it('should create a period that has a previous period', function () {
+            assert.equal(result.previousPeriod, period);
+        });
+
+        it('should set the time after the previous period', function () {
+            var startDate = period.startDate;
+            var thisStartDate = result.startDate;
+
+            var diff = Time.minusDates(thisStartDate, startDate);
+            
+            assert.equal(diff.getMinutes(), 20);
+        });
+    });
+
+    describe('#createFirstHalf()', function () {
         var result;
 
         beforeEach(function () {
@@ -25,10 +63,6 @@ describe('PeriodFactory', function () {
 
         it('should set the seed to firstHalf', function () {
             assert.equal(result.seed.getValue(), 'test,firstHalf');
-        });
-
-        it('should set the teams correctly', function () {
-            $
         });
     });
 });
