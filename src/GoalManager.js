28ca389751aaaf34f0d7718690a3937fce37eb41
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-08 22:44:01 +0000

    Updates syntax of goal manager to use class syntax, still failing

--- src/GoalManager.js
+++ src/GoalManager.js
@@ -2,25 +2,23 @@
  * Manages the goals scored, using an array containing the goals of the two
  * teams. Team A index 0, and Team B index 1.
  */
-function GoalManager(options) {
-    // Instances
-    this.teams = [
-        options.teamA,
-        options.teamB
-    ];
-    this.goals = [
-        [],
-        []
-    ];
-}
+class GoalManager {
+    constructor(options) {
+        this.teams = [
+            options.teamA,
+            options.teamB
+        ];
+        this.goals = [
+            [],
+            []
+        ];
+    }
 
-GoalManager.prototype = {
-    
     /*
      * Appends goals, that are passed in, to the this.goals array, depending on
      * the team index given.
      */
-    addGoals: function (teamIndex, goals) {
+    addGoals(teamIndex, goals) {
         var goalManager = new GoalManager({
             teamA: this.teams[0],
             teamB: this.teams[1]
@@ -36,14 +34,14 @@ GoalManager.prototype = {
             });
 
         return goalManager;
-    },
+    }
 
     /*
      * Appends all the goals, from the passed in GoalManager, to the goals in
      * this instance of GoalManager.  Doesn't pass in if teams, in the passed
      * in GoalManager and current GoalManager instance, are different.
      */
-    append: function (goalManager) {
+    append(goalManager) {
         var isNotTeamA = goalManager.teams[0] !== this.teams[0];
         var isNotTeamB = goalManager.teams[1] !== this.teams[1];
 
@@ -54,23 +52,23 @@ GoalManager.prototype = {
         return this
             .addGoals(0, goalManager.goals[0])
             .addGoals(1, goalManager.goals[1]);
-    },
+    }
 
     /*
      * Returns the score of the match as an array: [team A goals, team B
      * goals].
      */
-    getScore: function () {
+    getScore() {
         return [
             this.goals[0].length,
             this.goals[1].length
         ];
-    },
+    }
 
     /*
      * Gets the winner, returning their Team object. Returns null if draw.
      */
-    getWinner: function () {
+    getWinner() {
         var goals = this.goals;
 
         if (goals[0] > goals[1]) {
@@ -80,16 +78,15 @@ GoalManager.prototype = {
         }
 
         return null;
-    },
+    }
 
     /*
      * For each loop for every goal scored.
      */
-    forEach: function (callback) {
+    forEach(callback) {
         this.goals[0].forEach(callback);
         this.goals[1].forEach(callback);
     }
-
-};
+}
 
 module.exports = GoalManager;
