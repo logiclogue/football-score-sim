3f0eeff47f87c7114c545f7bd2d6e2279a715f3e
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-27 17:25:27 +0000

    Updates time to use class syntax

--- src/Time.js
+++ src/Time.js
@@ -1,5 +1,72 @@
-function Time(milliseconds) {
-    this.milliseconds = milliseconds || 0;
+class Time {
+    constructor(milliseconds) {
+        this.milliseconds = milliseconds || 0;
+        this.minusDates = Time.minusDates;
+    }
+
+    setMilliseconds(milliseconds) {
+        return new Time(milliseconds);
+    }
+
+    getMilliseconds() {
+        return this.milliseconds;
+    }
+
+    setSeconds(seconds) {
+        return new Time(seconds * 1000);
+    }
+
+    getSeconds() {
+        return this.milliseconds / 1000;
+    }
+
+    setMinutes(minutes) {
+        return new Time(minutes * 60000);
+    }
+
+    getMinutes() {
+        return this.milliseconds / 60000;
+    }
+
+    setHours(hours) {
+        return new Time(hours * 3600000);
+    }
+
+    getHours() {
+        return this.milliseconds / 3600000;
+    }
+
+    addToDate(date) {
+        var dateMilliseconds = date.getTime();
+        var newMilliseconds = dateMilliseconds + this.milliseconds;
+
+        return new Date(newMilliseconds);
+    }
+
+    minusFromDate(date) {
+        var dateMilliseconds = date.getTime();
+        var newMilliseconds = dateMilliseconds - this.milliseconds;
+
+        return new Date(newMilliseconds);
+    }
+
+    addTime(time) {
+        var milliA = time.getMilliseconds();
+        var milliB = this.getMilliseconds();
+
+        var sum = milliA + milliB;
+
+        return new Time(sum);
+    }
+
+    minusTime(time) {
+        var milliA = this.getMilliseconds();
+        var milliB = time.getMilliseconds();
+
+        var sum = milliA - milliB;
+
+        return new Time(sum);
+    }
 }
 
 Time.minusDates = function (dateA, dateB) {
@@ -8,76 +75,6 @@ Time.minusDates = function (dateA, dateB) {
     var milliDelta = dateAMilliseconds - dateBMilliseconds;
 
     return new Time(milliDelta);
-};
-
-Time.prototype = {
-
-    minusDates: Time.minusDates,
-
-    setMilliseconds: function (milliseconds) {
-        return new Time(milliseconds);
-    },
-
-    getMilliseconds: function () {
-        return this.milliseconds;
-    },
-
-    setSeconds: function (seconds) {
-        return new Time(seconds * 1000);
-    },
-
-    getSeconds: function () {
-        return this.milliseconds / 1000;
-    },
-
-    setMinutes: function (minutes) {
-        return new Time(minutes * 60000);
-    },
-
-    getMinutes: function () {
-        return this.milliseconds / 60000;
-    },
-
-    setHours: function (hours) {
-        return new Time(hours * 3600000);
-    },
-
-    getHours: function () {
-        return this.milliseconds / 3600000;
-    },
-
-    addToDate: function (date) {
-        var dateMilliseconds = date.getTime();
-        var newMilliseconds = dateMilliseconds + this.milliseconds;
-
-        return new Date(newMilliseconds);
-    },
-
-    minusFromDate: function (date) {
-        var dateMilliseconds = date.getTime();
-        var newMilliseconds = dateMilliseconds - this.milliseconds;
-
-        return new Date(newMilliseconds);
-    },
-
-    addTime: function (time) {
-        var milliA = time.getMilliseconds();
-        var milliB = this.getMilliseconds();
-
-        var sum = milliA + milliB;
-
-        return new Time(sum);
-    },
-
-    minusTime: function (time) {
-        var milliA = this.getMilliseconds();
-        var milliB = time.getMilliseconds();
-
-        var sum = milliA - milliB;
-
-        return new Time(sum);
-    }
-
 };
 
 module.exports = Time;
