78002b44fad754a152c283e2b4a2dc7d3baf6ab3
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-27 17:23:15 +0000

    Updates trapezium rule to use class syntax

--- src/TrapeziumRule.js
+++ src/TrapeziumRule.js
@@ -1,23 +1,25 @@
-function TrapeziumRule(width, precision) {
-    this.width = width;
-    this.precision = precision;
-}
-
-TrapeziumRule.prototype.getArea = function (f, startX, endX) {
-    var startY = f(startX);
-    var endY = f(endX);
-    var x;
-    var ySum = 0;
-    var result;
-
-    for (x = startX + this.width; x < endX; x += this.width) {
-        x = roundValue(x, this.precision);
-
-        ySum += f(x);
+class TrapeziumRule {
+    constructor(width, precision) {
+        this.width = width;
+        this.precision = precision;
     }
 
-    return 0.5 * this.width * (startY + endY + (2 * ySum));
-};
+    getArea(f, startX, endX) {
+        var startY = f(startX);
+        var endY = f(endX);
+        var x;
+        var ySum = 0;
+        var result;
+
+        for (x = startX + this.width; x < endX; x += this.width) {
+            x = roundValue(x, this.precision);
+
+            ySum += f(x);
+        }
+
+        return 0.5 * this.width * (startY + endY + (2 * ySum));
+    }
+}
 
 function roundValue(value, precision) {
     return Math.round(value * precision) / precision;
