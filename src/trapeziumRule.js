a766a26ee6a445a10cc5b94c02a74ca053ea07ac
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-28 19:07:54 +0000

    Adds trapeziumRule function

--- src/trapeziumRule.js
+++ src/trapeziumRule.js
@@ -1,6 +1,20 @@
+const roundValue = require("./roundValue");
+
 // Number -> Number -> (Number -> Number) -> Number -> Number
 function trapeziumRule(width, precision, f, startX, endX) {
-    
+    var startY = f(startX);
+    var endY = f(endX);
+    var x;
+    var ySum = 0;
+    var result;
+
+    for (x = startX + width; x < endX; x += width) {
+        x = roundValue(x, precision);
+
+        ySum += f(x);
+    }
+
+    return 0.5 * width * (startY + endY + (2 * ySum));
 }
 
 module.exports = trapeziumRule;
--- test/trapeziumRule-test.js
+++ test/trapeziumRule-test.js
@@ -2,9 +2,11 @@ const trapeziumRule = require("../src/trapeziumRule");
 const expect = require("chai").expect;
 
 describe("trapeziumRule()", () => {
-    context("(0.1, 10000, x = y, 0, 0)", () => {
-        it("", () => {
-            expect(false).to.be.true;
+    context("(0.1, 10000, x = y, 0, 1)", () => {
+        const result = trapeziumRule(0.1, 10000, x => x, 0, 1);
+
+        it("returns 0.5", () => {
+            expect(result).to.equal(0.5);
         });
     });
 });
