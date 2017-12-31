793f7e779f21be74d8a4e7855f7cbe5d6002432e
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-31 22:38:29 +0000

    Updates my sandbox

--- test/sandbox.js
+++ test/sandbox.js
@@ -9,10 +9,16 @@ describe("sandbox", () => {
     context("(map goals . relativeRatings) [1200, 800]", () => {
         it("returns goals", () => {
             const time = new Time().setMinutes(90);
-            const seed = "testing".toSeed();
+            const seed = Math.random().toString().toSeed();
             const result = _([1200, 800])
                 .thru(relativeRatings)
-                .map(rating => goals(rating, time, seed))
+                .map((ratingDiff, i) => {
+                    return {
+                        ratingDiff: ratingDiff,
+                        seed: seed.append(i)
+                    };
+                })
+                .map(o => goals(o.ratingDiff, time, o.seed))
                 .value();
 
             expect(result).to.deep.equal([3, 1]);
