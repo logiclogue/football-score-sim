3d7a3866417fc35a3e1c5da2e6ba1aac47b35726
logiclogue <jordanpaullord@hotmail.co.uk>
2018-01-10 21:27:30 +0000

    Removes index

--- index.js
+++
@@ -1,9 +0,0 @@
-var Team = require('./src/Team');
-var Match = require('./src/Match');
-var MatchOutputter = require('./src/MatchOutputter');
-
-module.exports = {
-    Team: Team,
-    Match: Match,
-    MatchOutputter: MatchOutputter
-}
