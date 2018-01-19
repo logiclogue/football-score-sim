1a36b21748bdbc8ca9854781897f3d6c285e7ccd
logiclogue <jordanpaullord@hotmail.co.uk>
2018-01-19 11:18:44 +0000

    Updates goals from ratings to use ratings diff and seed object

--- src/goalsFromRatings.js
+++ src/goalsFromRatings.js
@@ -2,29 +2,19 @@ const _ = require("lodash");
 const goals = require("./goals");
 const Ratings = require("./Ratings");
 const Goals = require("./Goals");
+const RatingDiffAndSeed = require("./RatingDiffAndSeed");
 
 // Ratings -> Time -> Seed -> Goals
 function goalsFromRatings(ratings, timeLength, seed) {
     return _(ratings.relative)
-        .map(_.curry(toDiffAndSeed)(ratings, seed))
+        .map(ratings.toRatingDiffAndSeed(seed))
         .map(toGoalsList(timeLength))
         .toGoals();
-}
-
-// Ratings -> Seed -> (Number -> Number -> DiffAndSeed)
-function toDiffAndSeed(ratings, seed, ratingDiff, i) {
-    return new DiffAndSeed(ratings, seed, ratingDiff, i);
 }
 
 // Time -> (DiffAndSeed -> [Number])
 function toGoalsList(timeLength) {
     return o => goals(o.ratingDiff, timeLength, o.seed);
-}
-
-// Ratings -> Seed -> Number -> Number -> DiffAndSeed
-function DiffAndSeed(ratings, seed, ratingDiff, i) {
-    this.ratingDiff = ratingDiff;
-    this.seed = seed.append(ratings.value).append(i);
 }
 
 // Ratings ~> Time -> Seed -> Goals
