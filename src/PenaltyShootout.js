fd058898411ad1a87a3a0b43f7040ffb7dad81df
logiclogue <jordanpaullord@hotmail.co.uk>
2018-02-12 22:21:54 +0000

    Adding penalty shootout class

---
+++ src/PenaltyShootout.js
@@ -0,0 +1,5 @@
+class PenaltyShootout {
+    
+}
+
+module.exports = PenaltyShootout;
---
+++ test/PenaltyShootout-test.js
@@ -0,0 +1,6 @@
+const expect = require("chai").expect;
+const PenaltyShootout = require("../src/PenaltyShootout");
+
+describe("PenaltyShootout", () => {
+    
+});
