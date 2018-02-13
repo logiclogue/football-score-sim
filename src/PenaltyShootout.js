864fecf9a9fe367a7bffc251ff4e1cb14e69b64c
logiclogue <jordanpaullord@hotmail.co.uk>
2018-02-13 21:54:14 +0000

    Adding attempt method, failing

--- src/PenaltyShootout.js
+++ src/PenaltyShootout.js
@@ -1,5 +1,23 @@
+const _ = require("lodash");
+
 class PenaltyShootout {
-    
+    // [[Boolean]] -> PenaltyShootout
+    constructor(record) {
+        this.record = record || [];
+    }
+
+    // PenaltyShootout ~> Number -> PenaltyShootout
+    attempt(index) {
+        const record = this.record.map((teamRecord, i) => {
+            if (i === index) {
+                return _.concat(teamRecord || [], true);
+            }
+
+            return teamRecord;
+        });
+
+        return new PenaltyShootout(record);
+    }
 }
 
 module.exports = PenaltyShootout;
--- test/PenaltyShootout-test.js
+++ test/PenaltyShootout-test.js
@@ -2,5 +2,13 @@ const expect = require("chai").expect;
 const PenaltyShootout = require("../src/PenaltyShootout");
 
 describe("PenaltyShootout", () => {
-    
+    describe("#attempt()", () => {
+        context("empty penalty shootout", () => {
+            it("simulates that penalty", () => {
+                const shootout = new PenaltyShootout();
+
+                expect(shootout.attempt(0).record[0]).to.deep.equal([true]);
+            });
+        });
+    });
 });
