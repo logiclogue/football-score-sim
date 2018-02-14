fef02d7fbd10103328fbd48d3dad40289863986a
logiclogue <jordanpaullord@hotmail.co.uk>
2018-02-14 22:18:15 +0000

    Fixes last, still lots of work and thinking to do, really struggling for time and motivation

--- src/PenaltyShootout.js
+++ src/PenaltyShootout.js
@@ -3,14 +3,14 @@ const _ = require("lodash");
 class PenaltyShootout {
     // [[Boolean]] -> PenaltyShootout
     constructor(record) {
-        this.record = record || [];
+        this.record = record || [[], []];
     }
 
     // PenaltyShootout ~> Number -> PenaltyShootout
     attempt(index) {
         const record = this.record.map((teamRecord, i) => {
             if (i === index) {
-                return _.concat(teamRecord || [], true);
+                return _.concat(teamRecord, true);
             }
 
             return teamRecord;
