0a13079b77997df3c5d5e590aec589c6caef6668
logiclogue <jordanpaullord@hotmail.co.uk>
2018-03-07 07:15:26 +0000

    Adds home and away getters

--- src/Match.js
+++ src/Match.js
@@ -1,9 +1,18 @@
 class Match {
     // [Team] -> Seed -> Match
     constructor(teams, seed) {
-        this.home = teams[0];
-        this.away = teams[1];
+        this.teams = teams;
         this.seed = seed;
+    }
+
+    // Match ~> Team
+    get home() {
+        return this.teams[0];
+    }
+
+    // Match ~> Team
+    get away() {
+        return this.teams[1];
     }
 }
 
