50cc41d0d63b3adead61c341e49a3d3a06288479
logiclogue <jordanpaullord@hotmail.co.uk>
2018-03-21 07:04:55 +0000

    Adding matchfinder stub function

---
+++ test/matchFinder.js
@@ -0,0 +1,10 @@
+// [Team] -> Seed -> (Match -> Boolean) -> Match
+function matchFinder(teams, seed, isMatch) {
+    matchFinderPrime(teams, seed, isMatch, 0);
+}
+
+function matchFinderPrime(teams, seed, isMatch, index) {
+
+}
+
+module.exports = matchFinder;
