1e9565ab8965119027fc3a6d08ac8f58a8762371
logiclogue <jordanpaullord@hotmail.co.uk>
2018-03-21 09:59:12 +0000

    Adds is penalty shootout for penalty shootout match

--- src/PenaltyShootoutMatch.js
+++ src/PenaltyShootoutMatch.js
@@ -1,11 +1,21 @@
-const Match = require("../Match");
+const Match = require("./Match");
 
 class PenaltyShootoutMatch extends Match {
     // Match -> PenaltyShootoutMatch
     constructor(match) {
-        super(match.teams, match.ratings);
+        super(match.teams, match.seed);
 
         this.match = match;
+    }
+
+    // @Override
+    // PenaltyShootoutMatch ~> Boolean
+    get isPenaltyShootout() {
+        if (this.goals.isDraw) {
+            return true;
+        }
+
+        return false;
     }
 }
 
--- test/PenaltyShootoutMatch-test.js
+++ test/PenaltyShootoutMatch-test.js
@@ -1,6 +1,31 @@
+const expect = require("chai").expect;
 const PenaltyShootoutMatch = require("../src/PenaltyShootoutMatch");
 const ExtraTimeMatch = require("../src/ExtraTimeMatch");
 const Match = require("../src/Match");
+const stubs = require("./stubs");
+const findMatch = require("./findMatch");
+const Seed = require("../src/Seed");
 
 describe("PenaltyShootoutMatch", () => {
+    describe("#isPenaltyShootout", () => {
+        context("given a drawn match", () => {
+            const isDrawn = match => match.goals.isDraw;
+            const normalMatch = findMatch(stubs.teams, isDrawn);
+            const match = new PenaltyShootoutMatch(normalMatch);
+
+            it("returns true", () => {
+                expect(match.isPenaltyShootout).to.be.true;
+            });
+        });
+
+        context("given a decided match", () => {
+            const isDecided = match => !match.goals.isDraw;
+            const normalMatch = findMatch(stubs.teams, isDecided);
+            const match = new PenaltyShootoutMatch(normalMatch);
+
+            it("returns false", () => {
+                expect(match.isPenaltyShootout).to.be.false;
+            });
+        });
+    });
 });
