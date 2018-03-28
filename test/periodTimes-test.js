b985a97419dc9945519f8c83353159cbfe910f11
logiclogue <jordanpaullord@hotmail.co.uk>
2018-03-28 22:22:19 +0100

    Updates period times test to find period with no goals

--- test/periodTimes-test.js
+++ test/periodTimes-test.js
@@ -4,15 +4,25 @@ const Time = require("../src/Time");
 const Ratings = require("../src/Ratings");
 const Seed = require("../src/Seed");
 const times = require("../src/times");
+const Match = require("../src/Match");
+const stubs = require("./stubs");
+const findMatch = require("./findMatch");
 
 describe("#periodTimes()", () => {
     const seed = "testing".toSeed();
     const time = new Time().setMinutes(45);
     const ratings = [1200, 1100].toRatings();
+    const period = findMatch(stubs.teams, isNilNil).firstHalf;
 
     context("given seed testing", () => {
         it("uses seed testing periodTimes", () => {
-            const expected = periodTimes()
+
         });
     });
 });
+
+function isNilNil(match) {
+    const result = match.firstHalf.goals.value;
+
+    return result[0] === 0 && result[1] === 0;
+}
