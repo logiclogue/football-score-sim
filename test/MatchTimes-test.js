a50bc05d21321372bc58855f258d3cbc3fa69de8
logiclogue <jordanpaullord@hotmail.co.uk>
2018-04-02 21:53:51 +0100

    Testing goal times with the match

--- test/MatchTimes-test.js
+++ test/MatchTimes-test.js
@@ -1,5 +1,13 @@
 const expect = require("chai").expect;
 const Match = require("../src/Match");
+const Seed = require("../src/Seed");
+const times = require("../src/times");
+const stubs = require("./stubs");
 
 describe("MatchTimes", () => {
+    const seed = "testing".toSeed();
+    const match = new Match(stubs.teams, seed);
+    const timesSeed = "goals".toSeed();
+
+    console.log(match.firstHalf.goals.times(match.firstHalf.timeLength, timesSeed).map(times => times.map(time => Math.floor(time.minutes))));
 });
