e25cd20e65b7f814a6b3a703419b92ff4cbec710
logiclogue <jordanpaullord@hotmail.co.uk>
2018-04-02 21:45:17 +0100

    Adds empty match times

---
+++ src/MatchTimes.js
@@ -0,0 +1,15 @@
+const Match = require("./Match");
+
+class MatchTimes {
+    // Match -> MatchTimes
+    constructor(match) {
+        this.value = match
+    }
+}
+
+// Match ~> MatchTimes
+Match.prototype.toMatchTimes = function () {
+    return new MatchTimes(this);
+};
+
+module.exports = MatchTimes;
---
+++ test/MatchTimes-test.js
@@ -0,0 +1,5 @@
+const expect = require("chai").expect;
+const Match = require("../src/Match");
+
+describe("MatchTimes", () => {
+});
