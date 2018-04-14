1ba7392037d7c704d2d484f8ffed3607257d1005
logiclogue <jordanpaullord@hotmail.co.uk>
2018-04-14 23:15:43 +0100

    Adds constructor for occurrence collection

--- src/OccurrenceCollection.js
+++ src/OccurrenceCollection.js
@@ -1,6 +1,15 @@
 class OccurrenceCollection {
-    constructor(goals) {
+    // Monoid a => a -> a -> a -> a -> OccurrenceCollection
+    constructor(goals, shotsOnTargetNoGoal, shotsOffTarget, shots) {
         this.goals = goals;
+        this.shotsOnTargetNoGoal = shotsOnTargetNoGoal;
+        this.shotsOffTarget = shotsOffTarget;
+        this.shots = shots;
+    }
+
+    // Monoid a => OccurrenceCollection ~> a
+    get shotsOnTarget() {
+
     }
 }
 
