7b65d19c16ffdb9c29b5ea9c02f33ded50ec1d5c
logiclogue <jordanpaullord@hotmail.co.uk>
2018-05-04 22:04:53 +0100

    Adds empty occurrence collection to times

---
+++ src/occurrenceCollectionToTimes.js
@@ -0,0 +1,13 @@
+const OccurrenceCollection = require("./OccurrenceCollection");
+
+// OccurrenceCollection Occurrences -> OccurrenceCollection OccurrenceTimes
+function occurrenceCollectionToTimes(collection) {
+    
+}
+
+// OccurrenceCollection Occurrences ~> OccurrenceCollection OccurrenceTimes
+OccurrenceCollection.prototype.toTimes = function () {
+
+};
+
+module.exports = occurrenceCollectionToTimes;
