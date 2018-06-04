d5d96fc6b3e27f22000e1f2d0c42654f9e83cc18
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-04 19:52:23 +0100

    Exports required objects

---
+++ index.js
@@ -0,0 +1,9 @@
+const Match = require("./src/Match");
+const Team = require("./src/Team");
+const Seed = require("./src/Seed");
+
+module.exports = {
+    Match: Match,
+    Team: Team,
+    Seed: Seed
+};
