93cf051a765cee303d2120d28fe5e16711d90038
logiclogue <jordanpaullord@hotmail.co.uk>
2019-01-02 18:43:16 +0000

    Tweaks draw constant to satisfy test

--- src/Elo.ml
+++ src/Elo.ml
@@ -7,7 +7,7 @@ let calculate_expected qa qb qc = qa /. (qa +. qb +. qc)
 let draw_rating rating_a rating_b =
     let average = (rating_a +. rating_b) /. 2.0 in
 
-    average -. 50.0
+    average -. 60.0
 
 let outcome_probabilities rating_a rating_b =
     let q_win  = calculate_q rating_a in
