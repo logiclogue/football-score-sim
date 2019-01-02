1e0acb8c90cb8469885a3c82063ebc6c24d44bba
logiclogue <jordanpaullord@hotmail.co.uk>
2019-01-02 18:20:06 +0000

    Adds some of the important elo functions

--- src/Elo.ml
+++ src/Elo.ml
@@ -1,3 +1,22 @@
 type q_val = float
+type probability = float
 
-let q rating = 10.0 ** (rating /. 400.0)
+let calculate_q rating = 10.0 ** (rating /. 400.0)
+
+let calculate_expected qa qb qc = qa /. (qa +. qb +. qc)
+
+let draw_rating rating_a rating_b =
+    let average = (rating_a +. rating_b) /. 2.0 in
+
+    average -. 50.0
+
+let outcome_probabilities rating_a rating_b =
+    let q_win  = calculate_q rating_a in
+    let q_draw = draw_rating rating_a rating_b |> calculate_q in
+    let q_loss = calculate_q rating_b in
+
+    let win_probability  = calculate_expected q_win q_draw q_loss in
+    let draw_probability = calculate_expected q_draw q_win q_loss in
+    let loss_probability = calculate_expected q_loss q_win q_draw in
+    
+    (win_probability, draw_probability, loss_probability)
--- src/Elo.mli
+++ src/Elo.mli
@@ -1,3 +1,10 @@
 type q_val
+type probability
 
-val q : Rating.t -> q_val
+val calculate_q : Rating.t -> q_val
+
+val calculate_expected : q_val -> q_val -> q_val -> probability
+
+val outcome_probabilities
+    : Rating.t -> Rating.t
+    -> probability * probability * probability
